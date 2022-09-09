07e6b293d34f1f43beadfd2e8be9252d22c7c3cf
adhasanblog <fhasanblog@gmail.com>
2022-09-09 16:47:11 +0700

    setting responsive menu

--- assets/js/web-config.js
+++ assets/js/web-config.js
@@ -1,16 +1,51 @@
 document.addEventListener('DOMContentLoaded', function () {
-  subMenuControler();
+  menuController();
+  subMenuController();
 });
 
-function subMenuControler() {
+function menuController() {
+  const menuMobileControl = document.querySelector('.menu-control-area');
+  const menuMobileControlSimbol = document.querySelector(
+    '.menu-control-area i'
+  );
+  const menuList = document.querySelector('nav .nav-list');
+
+  menuMobileControl.addEventListener('click', function () {
+    menuList.classList.toggle('active');
+
+    if (menuList.classList.contains('active')) {
+      menuMobileControlSimbol.className = 'fa-solid fa-xmark fa-xl ';
+    } else {
+      menuMobileControlSimbol.className = 'fa-solid fa-bars fa-xl';
+    }
+  });
+}
+
+function subMenuController() {
+  const menuListPrimary = document.querySelector('nav .nav-list');
   const menuItemsPrimary = document.querySelectorAll('nav ul .nav-item');
 
-  menuItemsPrimary.forEach(function (menuItem) {
-    menuItem.addEventListener('mouseover', function () {
-      menuItem.classList.add('active');
+  if (
+    window.matchMedia('(max-width: 719px) and (min-width: 300px)').matches ===
+    true
+  ) {
+    menuListPrimary.addEventListener('click', function (e) {
+      if (e.target.classList.contains('nav-item')) {
+        menuItemsPrimary.forEach(function (menuItem) {
+          menuItem.classList.remove('active');
+        });
+        e.target.classList.toggle('active');
+      }
+      console.log(e.target);
     });
-    menuItem.addEventListener('mouseout', function () {
-      menuItem.classList.remove('active');
+  } else {
+    menuItemsPrimary.forEach(function (menuItem) {
+      menuItem.addEventListener('mouseover', function () {
+        menuItem.classList.add('active');
+      });
+      menuItem.addEventListener('mouseout', function () {
+        menuItem.classList.remove('active');
+      });
     });
-  });
+  }
 }
